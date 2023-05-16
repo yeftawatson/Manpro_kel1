0779ecc6707e392cc2c8862740b00a9143b10e89
yeftawatson <yeftanathaniel16@gmail.com>
2023-05-16 15:27:42 +0700

    Update main2.py

--- Yefta Gambar to Gambar/main2.py
+++ Yefta Gambar to Gambar/main2.py
@@ -2,37 +2,37 @@ import cv2
 import numpy as np
 import os
 
-# Path to the directory containing the dataset images
+# Inisialisasi lokasi dataset
 dataset_path = 'dataset'
 
-# Load the image to compare
+# Load gambar input
 img_to_compare = cv2.imread('gambar1.jpg')
 
-# Convert the image to grayscale
+# convert gambar grayscale
 gray_img_to_compare = cv2.cvtColor(img_to_compare, cv2.COLOR_BGR2GRAY)
 
-# Calculate the histogram of the image to compare
+# Calculate histogram
 hist_to_compare = cv2.calcHist([gray_img_to_compare], [0], None, [256], [0, 256])
 cv2.normalize(hist_to_compare, hist_to_compare, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 
-# Loop through each image in the dataset directory
+# Looping membandingkan gambar selama gambar masih ada di dataset
 for filename in os.listdir(dataset_path):
     if filename.endswith('.jpg'):
-        # Load the image from the dataset directory
+        # Load gambar
         dataset_img = cv2.imread(os.path.join(dataset_path, filename))
         
-        # Convert the dataset image to grayscale
+        # Ubah menjadi grayscale
         gray_dataset_img = cv2.cvtColor(dataset_img, cv2.COLOR_BGR2GRAY)
         
-        # Calculate the histogram of the dataset image
+        # Hitung histogram
         hist_dataset_img = cv2.calcHist([gray_dataset_img], [0], None, [256], [0, 256])
         cv2.normalize(hist_dataset_img, hist_dataset_img, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
         
-        # Compare the histograms of the two images
+        # Membandingkan histogram
         score = cv2.compareHist(hist_to_compare, hist_dataset_img, cv2.HISTCMP_CORREL)
         
-        # Convert the score to a percentage
+        # Konversi nilai similarity
         percentage_score = score * 100
         
-        # Print the percentage score and the name of the dataset image
+        # Print output
         print(f'{filename}: {percentage_score:.2f}%')
