4c81a9ac2f1e5a225d62b69b9700cb625d18d5e6
gregoriussambirin <125953063+gregoriussambirin@users.noreply.github.com>
2023-05-28 17:49:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,299 +1,200 @@
 
 
 $(function(){
-  $(".toggle").on("click", function(){
-      if($(".menu").hasClass("active")){
-          $(".menu").removeClass("active");
-          $(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>");
-      
-      } else {
-          $(".menu").addClass("active");
-          $(this).find("a").html("<ion-icon name='close-outline'></ion-icon>");
-      }
-
-  });
-});
-
-
-
-jQuery(document).ready(function($){
-    var $form_modal = $('.user-modal'),
-      $form_login = $form_modal.find('#login'),
-      $form_signup = $form_modal.find('#signup'),
-      $form_forgot_password = $form_modal.find('#reset-password'),
-      $form_modal_tab = $('.switcher'),
-      $tab_login = $form_modal_tab.children('li').eq(0).children('a'),
-      $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
-      $forgot_password_link = $form_login.find('.form-bottom-message a'),
-      $back_to_login_link = $form_forgot_password.find('.form-bottom-message a'),
-      $main_nav = $('.main-nav');
-      $signup = $('.signup');
-      $signin = $('.signin');
-  
-    //open modal
-    $signup.on('click', function(event){
-  
-      if( $(event.target).is($main_nav) ) {
-        // on mobile open the submenu
-        $(this).children('ul').toggleClass('is-visible');
-      } else {
-        // on mobile close submenu
-        $main_nav.children('ul').removeClass('is-visible');
-        //show modal layer
-        $form_modal.addClass('is-visible'); 
-        //show the selected form
-        ( $(event.target).is('.signup') ) ? signup_selected() : login_selected();
-      }
-  
-    });
-
-    $signin.on('click', function(event){
-  
-      if( $(event.target).is($main_nav) ) {
-        // on mobile open the submenu
-        $(this).children('ul').toggleClass('is-visible');
-      } else {
-        // on mobile close submenu
-        $main_nav.children('ul').removeClass('is-visible');
-        //show modal layer
-        $form_modal.addClass('is-visible'); 
-        //show the selected form
-        ( $(event.target).is('.signin') ) ? login_selected() : login_selected();
-      }
-  
-    });
-  
-    //close modal
-    $('.user-modal').on('click', function(event){
-      if( $(event.target).is($form_modal) || $(event.target).is('.close-form') ) {
-        $form_modal.removeClass('is-visible');
-      } 
-    });
-
-    
-
-    //close modal when clicking the esc keyboard button
-    $(document).keyup(function(event){
-        if(event.which=='27'){
-          $form_modal.removeClass('is-visible');
+    $(".toggle").on("click", function(){
+        if($(".menu").hasClass("active")){
+            $(".menu").removeClass("active");
+            $(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>");
+        
+        } else {
+            $(".menu").addClass("active");
+            $(this).find("a").html("<ion-icon name='close-outline'></ion-icon>");
         }
+  
+    });
+  });
+  
+  
+  
+  jQuery(document).ready(function($){
+      var $form_modal = $('.user-modal'),
+        $form_login = $form_modal.find('#login'),
+        $form_signup = $form_modal.find('#signup'),
+        $form_forgot_password = $form_modal.find('#reset-password'),
+        $form_modal_tab = $('.switcher'),
+        $tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+        $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+        $forgot_password_link = $form_login.find('.form-bottom-message a'),
+        $back_to_login_link = $form_forgot_password.find('.form-bottom-message a'),
+        $main_nav = $('.main-nav');
+        $signup = $('.signup');
+        $signin = $('.signin');
+    
+      //open modal
+      $signup.on('click', function(event){
+    
+        if( $(event.target).is($main_nav) ) {
+          // on mobile open the submenu
+          $(this).children('ul').toggleClass('is-visible');
+        } else {
+          // on mobile close submenu
+          $main_nav.children('ul').removeClass('is-visible');
+          //show modal layer
+          $form_modal.addClass('is-visible'); 
+          //show the selected form
+          ( $(event.target).is('.signup') ) ? signup_selected() : login_selected();
+        }
+    
       });
   
-    //switch from a tab to another
-    $form_modal_tab.on('click', function(event) {
-      event.preventDefault();
-      ( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
-    });
+      $signin.on('click', function(event){
+    
+        if( $(event.target).is($main_nav) ) {
+          // on mobile open the submenu
+          $(this).children('ul').toggleClass('is-visible');
+        } else {
+          // on mobile close submenu
+          $main_nav.children('ul').removeClass('is-visible');
+          //show modal layer
+          $form_modal.addClass('is-visible'); 
+          //show the selected form
+          ( $(event.target).is('.signin') ) ? login_selected() : login_selected();
+        }
+    
+      });
+    
+      //close modal
+      $('.user-modal').on('click', function(event){
+        if( $(event.target).is($form_modal) || $(event.target).is('.close-form') ) {
+          $form_modal.removeClass('is-visible');
+        } 
+      });
   
-    //hide or show password
-    $('.hide-password').on('click', function(){
-      var $this= $(this),
-        $password_field = $this.prev('input');
       
-      ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
-      ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
-      //focus and move cursor to the end of input field
-      $password_field.putCursorAtEnd();
-    });
   
-    //show forgot-password form 
-    $forgot_password_link.on('click', function(event){
-      event.preventDefault();
-      forgot_password_selected();
-    });
-  
-    //back to login from the forgot-password form
-    $back_to_login_link.on('click', function(event){
-      event.preventDefault();
-      login_selected();
-    });
-  
-    function login_selected(){
-      $form_login.addClass('is-selected');
-      $form_signup.removeClass('is-selected');
-      $form_forgot_password.removeClass('is-selected');
-      $tab_login.addClass('selected');
-      $tab_signup.removeClass('selected');
-    }
-  
-    function signup_selected(){
-      $form_login.removeClass('is-selected');
-      $form_signup.addClass('is-selected');
-      $form_forgot_password.removeClass('is-selected');
-      $tab_login.removeClass('selected');
-      $tab_signup.addClass('selected');
-    }
-  
-    function forgot_password_selected(){
-      $form_login.removeClass('is-selected');
-      $form_signup.removeClass('is-selected');
-      $form_forgot_password.addClass('is-selected');
-    }
-  
-    //REMOVE THIS - it's just to show error messages 
-    $form_login.find('input[type="submit"]').on('click', function(event){
-      event.preventDefault();
-      $form_login.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
-    });
-    $form_signup.find('input[type="submit"]').on('click', function(event){
-      event.preventDefault();
-      $form_signup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
-    });
-  
-  
-    //IE9 placeholder fallback
-    //credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
-    if(!Modernizr.input.placeholder){
-      $('[placeholder]').focus(function() {
-        var input = $(this);
-        if (input.val() == input.attr('placeholder')) {
-          input.val('');
+      //close modal when clicking the esc keyboard button
+      $(document).keyup(function(event){
+          if(event.which=='27'){
+            $form_modal.removeClass('is-visible');
           }
-      }).blur(function() {
-        var input = $(this);
-          if (input.val() == '' || input.val() == input.attr('placeholder')) {
-          input.val(input.attr('placeholder'));
-          }
-      }).blur();
-      $('[placeholder]').parents('form').submit(function() {
-          $(this).find('[placeholder]').each(function() {
+        });
+    
+      //switch from a tab to another
+      $form_modal_tab.on('click', function(event) {
+        event.preventDefault();
+        ( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
+      });
+    
+      //hide or show password
+      $('.hide-password').on('click', function(){
+        var $this= $(this),
+          $password_field = $this.prev('input');
+        
+        ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+        ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
+        //focus and move cursor to the end of input field
+        $password_field.putCursorAtEnd();
+      });
+    
+      //show forgot-password form 
+      $forgot_password_link.on('click', function(event){
+        event.preventDefault();
+        forgot_password_selected();
+      });
+    
+      //back to login from the forgot-password form
+      $back_to_login_link.on('click', function(event){
+        event.preventDefault();
+        login_selected();
+      });
+    
+      function login_selected(){
+        $form_login.addClass('is-selected');
+        $form_signup.removeClass('is-selected');
+        $form_forgot_password.removeClass('is-selected');
+        $tab_login.addClass('selected');
+        $tab_signup.removeClass('selected');
+      }
+    
+      function signup_selected(){
+        $form_login.removeClass('is-selected');
+        $form_signup.addClass('is-selected');
+        $form_forgot_password.removeClass('is-selected');
+        $tab_login.removeClass('selected');
+        $tab_signup.addClass('selected');
+      }
+    
+      function forgot_password_selected(){
+        $form_login.removeClass('is-selected');
+        $form_signup.removeClass('is-selected');
+        $form_forgot_password.addClass('is-selected');
+      }
+    
+      //REMOVE THIS - it's just to show error messages 
+      $form_login.find('input[type="submit"]').on('click', function(event){
+        event.preventDefault();
+        $form_login.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+      });
+      $form_signup.find('input[type="submit"]').on('click', function(event){
+        event.preventDefault();
+        $form_signup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+      });
+    
+    
+      //IE9 placeholder fallback
+      //credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+      if(!Modernizr.input.placeholder){
+        $('[placeholder]').focus(function() {
           var input = $(this);
           if (input.val() == input.attr('placeholder')) {
             input.val('');
-          }
-          })
-      });
-    }
-  
-  });
-  
-  
-  //credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
-  jQuery.fn.putCursorAtEnd = function() {
-    return this.each(function() {
-        // If this function exists...
-        if (this.setSelectionRange) {
-            // ... then use it (Doesn't work in IE)
-            // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-            var len = $(this).val().length * 2;
-            this.setSelectionRange(len, len);
-        } else {
-          // ... otherwise replace the contents with itself
-          // (Doesn't work in Google Chrome)
-            $(this).val($(this).val());
-        }
+            }
+        }).blur(function() {
+          var input = $(this);
+            if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            input.val(input.attr('placeholder'));
+            }
+        }).blur();
+        $('[placeholder]').parents('form').submit(function() {
+            $(this).find('[placeholder]').each(function() {
+            var input = $(this);
+            if (input.val() == input.attr('placeholder')) {
+              input.val('');
+            }
+            })
+        });
+      }
+    
     });
-  };
+    
+    
+    //credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+    jQuery.fn.putCursorAtEnd = function() {
+      return this.each(function() {
+          // If this function exists...
+          if (this.setSelectionRange) {
+              // ... then use it (Doesn't work in IE)
+              // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+              var len = $(this).val().length * 2;
+              this.setSelectionRange(len, len);
+          } else {
+            // ... otherwise replace the contents with itself
+            // (Doesn't work in Google Chrome)
+              $(this).val($(this).val());
+          }
+      });
+    };
 
-  // MESSAGE INPUT
-const textarea = document.querySelector('.chatbox-message-input')
-const chatboxForm = document.querySelector('.chatbox-message-form')
+  
+  
+  
 
-textarea.addEventListener('input', function () {
-	let line = textarea.value.split('\n').length
+  
+  
 
-	if(textarea.rows < 6 || line < 6) {
-		textarea.rows = line
-	}
-
-	if(textarea.rows > 1) {
-		chatboxForm.style.alignItems = 'flex-end'
-	} else {
-		chatboxForm.style.alignItems = 'center'
-	}
-})
-
-
-
-// TOGGLE CHATBOX
-const chatboxToggle = document.querySelector('.chatbox-toggle')
-const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
-
-chatboxToggle.addEventListener('click', function () {
-	chatboxMessage.classList.toggle('show')
-})
-
-
-
-// DROPDOWN TOGGLE
-const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle')
-const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu')
-
-dropdownToggle.addEventListener('click', function () {
-	dropdownMenu.classList.toggle('show')
-})
-
-document.addEventListener('click', function (e) {
-	if(!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
-		dropdownMenu.classList.remove('show')
-	}
-})
-
-
-
-
-
-
-
-// CHATBOX MESSAGE
-const chatboxMessageWrapper = document.querySelector('.chatbox-message-content')
-const chatboxNoMessage = document.querySelector('.chatbox-message-no-message')
-
-chatboxForm.addEventListener('submit', function (e) {
-	e.preventDefault()
-
-	if(isValid(textarea.value)) {
-		writeMessage()
-		setTimeout(autoReply, 1000)
-	}
-})
-
-
-
-function addZero(num) {
-	return num < 10 ? '0'+num : num
-}
-
-function writeMessage() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item sent">
-			<span class="chatbox-message-item-text">
-				${textarea.value.trim().replace(/\n/g, '<br>\n')}
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	chatboxForm.style.alignItems = 'center'
-	textarea.rows = 1
-	textarea.focus()
-	textarea.value = ''
-	chatboxNoMessage.style.display = 'none'
-	scrollBottom()
-}
-
-function autoReply() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item received">
-			<span class="chatbox-message-item-text">
-				Thank you for your awesome support!
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	scrollBottom()
-}
-
-function scrollBottom() {
-	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
-}
-
-function isValid(value) {
-	let text = value.replace(/\n/g, '')
-	text = text.replace(/\s/g, '')
-
-	return text.length > 0
-}
+  
+  
+  
+  
+  
+  
